d153708fd7cb200c67671053269fe5f8bb423f6f
abhilash <abhilash.gupt@yahoo.com>
2023-05-09 23:21:01 +0530

    backend basic setup complete

---
+++ Backend/src/main/java/com/hollowambition/contenttrackerws/ContentTrackerWSApplication.java
@@ -0,0 +1,12 @@
+package com.hollowambition.contenttrackerws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentTrackerWSApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ContentTrackerWSApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/hollowambition/contenttrackerws/ContenttrackerwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollowambition.contenttrackerws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContenttrackerwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
